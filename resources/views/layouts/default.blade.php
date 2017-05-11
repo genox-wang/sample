b5b68f5e6a3dea69ead82d76c9df3be66e210307
loostudy <genius840215@163.com>
2017-05-11 07:07:04 +0000

    Add Static Pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title>@yield('title','Simple') -- Laravel 入门教程</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
